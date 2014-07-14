14498ef81bd6fad9d1071b90c86b22d8bd723cde
rasbabbo <rasbabbo@yahoo.com>
2014-07-14 07:55:20 -0700

    trying to git arrayto work

--- tictactoe.js
+++ tictactoe.js
@@ -3,26 +3,32 @@ window.onload = function(){
 //	FUNCTION FOR SELECTING BETWEEN X'S AND O'S
 	var taco = true
 	var boxes = document.getElementsByClassName("box");
+	var holder = [0,0,0,0,0,0,0,0,0];
 
 	for (i = 0; i < boxes.length; i++) {
 			boxes[i].onclick = function() {
+				
+
 				if (this.innerHTML === "O") {
 					alert("Pick another square hombre!");
 				} else {
 					if (taco === true ) {
 						this.innerHTML = "X";
-						this.style.color = "red"
+						this.style.color = "red";
+						holder.splice(this.i, 1, "X");
 						taco = false;
 
 			} 	else 	{ 
 					while (taco === false ) {
 						this.innerHTML = "O";
 						this.style.color = "blue";
-							taco = true;
+						holder.splice(this.i, 1, "O");
+						taco = true;
 
 						}
 					}
-				}  
+				} 
+				console.log(holder) 
 			} 
 		}
 
@@ -37,6 +43,5 @@ window.onload = function(){
 		}
 	};
 // COLOR PICKER - its OK to pick your nose, and your colors and your friends colors, but...
-
+console.log(holder)
 }
-
